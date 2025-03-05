3452dc569806834647bbcb4e79e443ed47f542d3
ettabet aicha <aicha70tabite@gmail.com>
2025-03-05 10:49:37 +0000

    define hotel interface

---
+++ src/app/core/models/hotel.interface.ts
@@ -0,0 +1,16 @@
+export interface Hotel {
+  id: number
+  name: string
+  location: string
+  price: number
+  originalPrice?: number
+  discount?: number
+  rating: number
+  reviewCount: number
+  image: string
+  amenities: string[]
+  stars: number
+  isFavorite: boolean
+  distance?: string
+  description: string
+}
