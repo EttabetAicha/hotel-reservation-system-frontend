55e378012cf52b761a6fb9b8b73d1460971d8c11
ettabet aicha <aicha70tabite@gmail.com>
2025-03-06 10:23:14 +0000

    add interface of user review

---
+++ src/app/core/models/review.interface.ts
@@ -0,0 +1,13 @@
+export interface Review {
+  id: number
+  image:String
+  author: string
+  date: string
+  rating: number
+  title: string
+  comment: string
+  pros?: string
+  cons?: string
+  stayType: string
+}
+
